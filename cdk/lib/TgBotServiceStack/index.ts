e103ebce4189caeee097b09bc502005b459dc72d
restificable <nostakeengineer@gmail.com>
2022-07-12 00:40:11 +0800

    cdk: Using v2 style of cross-stack reference #1

--- cdk/lib/TgBotServiceStack/index.ts
+++ cdk/lib/TgBotServiceStack/index.ts
@@ -1,11 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { PingApi } from './pingApi';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+
+interface TgBotServiceStackProps extends StackProps {
+  readonly pingTable: Table;
+}
 
 export class TgBotServiceStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: TgBotServiceStackProps) {
+    const {pingTable} = props;
     super(scope, id, props);
 
-    new PingApi(this, "PingApi");
+    new PingApi(this, "PingApi", {
+      pingTable,
+    });
   }
 }
